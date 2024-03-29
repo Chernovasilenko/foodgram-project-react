b6f56522e8637b5db7e1ae9a7514939c62ede327
Chernovasilenko <a.chernovasilenko@yandex.ru>
2024-03-29 07:56:46 +0500

    add createsuperuser

--- backend/recipes/management/commands/load_recipes.py
+++ backend/recipes/management/commands/load_recipes.py
@@ -1,15 +1,24 @@
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
+from django.db.utils import IntegrityError
 
 from recipes.models import Tag
+
+DATA_TAGS = [
+    {'name': 'Завтрак', 'color': '#FF0000', 'slug': 'breakfast'},
+    {'name': 'Обед', 'color': '#00FF00', 'slug': 'lunch'},
+    {'name': 'Ужин', 'color': '#0000FF', 'slug': 'dinner'}]
 
 
 class Command(BaseCommand):
     help = 'Добавление стандартных тегов для рецептов в БД'
 
     def handle(self, *args, **kwargs):
-        data = [
-            {'name': 'Завтрак', 'color': '#FF0000', 'slug': 'breakfast'},
-            {'name': 'Обед', 'color': '#00FF00', 'slug': 'lunch'},
-            {'name': 'Ужин', 'color': '#0000FF', 'slug': 'dinner'}]
-        Tag.objects.bulk_create(Tag(**tag) for tag in data)
-        self.stdout.write(self.style.SUCCESS('Тэги загружены.'))
+        try:
+            Tag.objects.bulk_create(Tag(**tag) for tag in DATA_TAGS)
+            self.stdout.write(self.style.SUCCESS('Тэги загружены.'))
+        except IntegrityError as e:
+            raise CommandError(
+                f'При загрузке данных произошла ошибка: {e}\n'
+                'Данные, которые вы пытаетесь загрузить, уже есть в таблице'
+            )
+
